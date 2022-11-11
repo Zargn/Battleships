448d61bc5c84ac2464a39d275754c351b7f383a2
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-11 16:01:18 +0100

    Game.cs: Added todo.

--- Battleships/Game.cs
+++ Battleships/Game.cs
@@ -2,8 +2,22 @@
 
 public class Game
 {
-    public static void Main()
+    public static async Task Main()
     {
-        
+        var game = new Game();
+        await game.Run();
+    }
+
+    public async Task Run()
+    {
+        // Todo in order:
+        // Welcome player.
+        // Let player pick game mode. vs ai, or vs remote player.
+        // Instantiate the player.
+        // Instantiate the remote/ai player.
+        // Decide who starts.
+        // PlayTurn on the starting player.
+        // PlayTurn on the other player.
+        // Repeat last two steps until one player is defeated or something breaks.
     }
 }
