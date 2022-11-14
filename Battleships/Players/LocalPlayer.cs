bbfb206f645e8b60aaf8494cf6ccf5632660e00a
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-14 17:15:26 +0100

    LocalPlayer.cs: Began work.

---
+++ Battleships/Players/LocalPlayer.cs
@@ -0,0 +1,28 @@
+﻿using Battleships.EventArguments;
+using Battleships.Interfaces;
+using Battleships.objects;
+
+namespace Battleships.Players;
+
+public class LocalPlayer : IPlayer
+{
+    private Arena arena;
+    
+    public Task InitializePlayer(Arena arena, CancellationToken cancellationToken)
+    {
+        this.arena = arena;
+        throw new NotImplementedException();
+    }
+
+    public Task PlayTurn()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Tile> HitTile(FiringTarget firingTarget)
+    {
+        throw new NotImplementedException();
+    }
+
+    public event EventHandler<PlayerDefeatedEventArgs>? PlayerDefeated;
+}
