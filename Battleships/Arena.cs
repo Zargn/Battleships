38deacd4ba57741f89f94dc05cc255473b9c24f2
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-14 17:15:47 +0100

    Arena.cs: Added FireAtTile Method & properties

--- Battleships/Arena.cs
+++ Battleships/Arena.cs
@@ -6,8 +6,18 @@ public class Arena
 {
     private Tile[,] tiles;
 
+    public int XSize => tiles.GetLength(0);
+    public int YSize => tiles.GetLength(1);
+    
     public Arena(int xSize, int ySize)
     {
         tiles = new Tile[xSize, ySize];
+    }
+
+    // Fires at the target x and y coordinates.
+    public bool FireAtTile(int x, int y)
+    {
+        tiles[x, y].Hit = true;
+        return tiles[x, y].OccupiedByShip;
     }
 }
