4d98c6f66b41d02edcd682c5e8d3085377ff910e
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-17 17:21:18 +0100

    Game.cs: Started work on GameLoop

--- Battleships/Game.cs
+++ Battleships/Game.cs
@@ -1,4 +1,6 @@
-﻿namespace Battleships;
+﻿using Battleships.Interfaces;
+
+namespace Battleships;
 
 public class Game
 {
@@ -7,6 +9,8 @@ public class Game
         var game = new Game();
         await game.Run();
     }
+
+
 
     public async Task Run()
     {
@@ -19,5 +23,32 @@ public class Game
         // PlayTurn on the starting player.
         // PlayTurn on the other player.
         // Repeat last two steps until one player is defeated or something breaks.
+    }
+
+    private Task InitializeGame()
+    {
+        
+    }
+
+    private Task GameLoop(IPlayer player1, IPlayer player2)
+    {
+        IPlayer[] players = ConfigurePlayerOrder(player1, player2);
+    }
+
+    private IPlayer[] ConfigurePlayerOrder(IPlayer player1, IPlayer player2)
+    {
+        IPlayer[] players = new IPlayer[2];
+
+        if (player1.PlayerStartPriority > player2.PlayerStartPriority)
+        {
+            players = new []{player1, player2};
+            return players;
+        }
+
+        if (player1.PlayerStartPriority < player2.PlayerStartPriority)
+        {
+            players = new []{player2, player1};
+            return players;
+        }
     }
 }
