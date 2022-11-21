73b1c4566b00ce653fb633165eaba7418e267f84
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-21 17:46:58 +0100

    Ship.cs: Added health.

--- Battleships/objects/Ship.cs
+++ Battleships/objects/Ship.cs
@@ -7,11 +7,13 @@ public class Ship
         Length = length;
         Name = name;
         Type = type;
+        Health = length;
         CoordinatesArray = new TargetCoordinates[length];
     }
 
     public int Length { get; }
     public string Name { get; }
     public string Type { get; }
+    public int Health { get; set; }
     public TargetCoordinates[] CoordinatesArray { get; }
 }
