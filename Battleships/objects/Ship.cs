eb18558174e6eeb232a16a2a38da0e02279e7ed1
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-21 17:12:56 +0100

    Ship.cs: Added constructor and Coordinate array

--- Battleships/objects/Ship.cs
+++ Battleships/objects/Ship.cs
@@ -2,7 +2,16 @@
 
 public class Ship
 {
+    public Ship(int length, string name, string type)
+    {
+        Length = length;
+        Name = name;
+        Type = type;
+        CoordinatesArray = new TargetCoordinates[length];
+    }
+
     public int Length { get; }
     public string Name { get; }
     public string Type { get; }
+    public TargetCoordinates[] CoordinatesArray { get; }
 }
