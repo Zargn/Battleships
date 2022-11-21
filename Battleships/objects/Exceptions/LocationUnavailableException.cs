572bd0314559ff1476bf8b45d156d7ddcd060e6f
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-21 16:14:39 +0100

    Create LocationUnavailableException.cs

---
+++ Battleships/objects/Exceptions/LocationUnavailableException.cs
@@ -0,0 +1,19 @@
+﻿namespace Battleships.objects.Exceptions;
+
+public class LocationUnavailableException : Exception
+{
+    public LocationUnavailableException()
+    {
+        
+    }
+
+    public LocationUnavailableException(string message) : base(message)
+    {
+        
+    }
+
+    public LocationUnavailableException(string message, Exception inner) : base(message, inner)
+    {
+        
+    }
+}
