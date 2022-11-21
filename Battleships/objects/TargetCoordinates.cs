da6e3b53899fcbc89a326a0e48591cc9b1335cfc
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-21 16:12:36 +0100

    TargetCoordinates.cs: Added Direction statics

--- Battleships/objects/TargetCoordinates.cs
+++ Battleships/objects/TargetCoordinates.cs
@@ -10,4 +10,9 @@ public struct TargetCoordinates
         XCoordinate = xCoordinate;
         YCoordinate = yCoordinate;
     }
+
+    public static TargetCoordinates North => new TargetCoordinates(0, 1);
+    public static TargetCoordinates East => new TargetCoordinates(1, 0);
+    public static TargetCoordinates South => new TargetCoordinates(0, -1);
+    public static TargetCoordinates West => new TargetCoordinates(-1, 0);
 }
