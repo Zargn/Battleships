afefdeb1cac5a833d9dd524709309c4696798d4d
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-22 11:33:02 +0100

    Create ShipPlacementInformation.cs

---
+++ Battleships/objects/ShipPlacementInformation.cs
@@ -0,0 +1,15 @@
+﻿namespace Battleships.objects;
+
+public class ShipPlacementInformation
+{
+    public TargetCoordinates Coordinates { get; }
+    public TargetCoordinates Direction { get; }
+    public string Name { get; }
+
+    public ShipPlacementInformation(TargetCoordinates coordinates, TargetCoordinates direction, string name)
+    {
+        Coordinates = coordinates;
+        Direction = direction;
+        Name = name;
+    }
+}
