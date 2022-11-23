50a077c6fd2b7eb760d0ec18c51806658e97e2a2
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-23 18:29:00 +0100

    ConsoleUserInterface.cs: Finished ship placement

--- Battleships/ConsoleUserInterface.cs
+++ Battleships/ConsoleUserInterface.cs
@@ -79,6 +79,12 @@ public class ConsoleUserInterface : IUserInterface
         var coordinates = GetTargetCoordinates();
 
         Console.WriteLine($"Please enter direction: ");
+        var direction = GetDirection();
+
+        Console.WriteLine("Please enter Ship Name. (Leave empty for default.)");
+        var name = Console.ReadLine();
+
+        return new ShipPlacementInformation(coordinates, direction, name);
     }
 
     public void DisplayError(string message)
@@ -98,6 +104,34 @@ public class ConsoleUserInterface : IUserInterface
             catch (FormatException)
             {
                 Console.WriteLine("Input was in wrong format. Please enter coordinates in the following format: (Xvalue Yvalue)");
+            }
+        }
+    }
+
+    private TargetCoordinates GetDirection()
+    {
+        Console.WriteLine("Please enter direction: (North, East, South, West)");
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (input == null)
+            {
+                Console.WriteLine("Please enter one of the following: (North, East, South, West)");
+                continue;
+            }
+            switch (input[0])
+            {
+                case 'n':
+                    return TargetCoordinates.North;
+                case 'e':
+                    return TargetCoordinates.East;
+                case 's':
+                    return TargetCoordinates.South;
+                case 'w':
+                    return TargetCoordinates.West;
+                default:
+                    Console.WriteLine("Please enter one of the following: (North, East, South, West)");
+                    break;
             }
         }
     }
