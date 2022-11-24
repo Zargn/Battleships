a3568c3a685f5ad99e419690da77b15343455d30
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-24 16:17:29 +0100

    Imported ILogger & IJsonSerializer Implementers.

---
+++ Battleships/objects/networking/ConsoleLogger.cs
@@ -0,0 +1,11 @@
+﻿using Unnamed_Networking_Plugin.Interfaces;
+
+namespace Battleships.objects.networking;
+
+public class ConsoleLogger : ILogger
+{
+    public void Log(object sender, string message, LogType logType)
+    {
+        Console.WriteLine($"[{logType.ToString()}] {sender}: {message}");
+    }
+}
