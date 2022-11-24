e13e9d9a3bd849491260d8da6ab547f3448f194c
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-24 16:08:33 +0100

    Create OnlineUserIdentification data and package.

--- Battleships/objects/networking/OnlineUserIdentification.cs
+++ Battleships/objects/networking/OnlineUserIdentification.cs
@@ -1,6 +1,46 @@
-﻿namespace Battleships.objects.networking;
+﻿using Unnamed_Networking_Plugin.Interfaces;
+using Unnamed_Networking_Plugin.Resources;
 
-public class OnlineUserIdentification
+namespace Battleships.objects.networking;
+
+public class OnlineUserIdentificationPackage : IdentificationPackage
 {
+    public OnlineUserIdentification OnlineUserIdentification { get; }
+
+    public OnlineUserIdentificationPackage(OnlineUserIdentification onlineUserIdentification)
+    {
+        OnlineUserIdentification = onlineUserIdentification;
+    }
     
+    public override IConnectionInformation ExtractConnectionInformation()
+    {
+        return OnlineUserIdentification;
+    }
+}
+
+public class OnlineUserIdentification : IConnectionInformation
+{ 
+    public string UserName { get; init; }
+    
+    public OnlineUserIdentification(string userName)
+    {
+        UserName = userName;
+    }
+    
+    public override bool Equals(object? obj)
+    {
+        if (obj is OnlineUserIdentification)
+            return (obj as OnlineUserIdentification).UserName == UserName;
+        return false;
+    }
+    
+    public override string ToString()
+    {
+        return UserName;
+    }
+    
+    public override int GetHashCode()
+    {
+        return UserName.GetHashCode();
+    }
 }
