5d00113635aea692b8ff7c192e5b3ddc2e89c9cf
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-24 17:16:31 +0100

    RemotePlayer.cs: Started work on initialization.

--- Battleships/Players/RemotePlayer.cs
+++ Battleships/Players/RemotePlayer.cs
@@ -2,6 +2,7 @@
 using Battleships.Interfaces;
 using Battleships.objects;
 using Battleships.objects.Enums;
+using Battleships.objects.networking;
 using ForwardingClient;
 
 namespace Battleships.Players;
@@ -26,13 +27,50 @@ public class RemotePlayer : IPlayer
     
     
     
-    public Task InitializePlayer(int[] shipLengths, int xSize, int ySize, CancellationToken cancellationToken)
+    public async Task InitializePlayer(int[] shipLengths, int xSize, int ySize, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        netClient = await ConnectToServer(cancellationToken);
+        await ListGroups();
     }
-    
-    
-    
+
+    private async Task<FwClient> ConnectToServer(CancellationToken cancellationToken)
+    {
+        FwClient? client = null;
+        bool success = false;
+        while (!success)
+        {
+            if (!cancellationToken.IsCancellationRequested)
+                throw new OperationCanceledException();
+            
+            var allowedLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var letterArray = new char[5];
+            for (int i = 0; i < letterArray.Length; i++)
+                letterArray[i] = allowedLetters[Random.Shared.Next(allowedLetters.Length)];
+            string groupCode = new string(letterArray);
+            var identification = new OnlineUserIdentification(groupCode);
+            var identificationPackage = new OnlineUserIdentificationPackage(identification);
+
+            client = new FwClient(new ConsoleLogger(), new JsonSerializerAdapter(), identificationPackage);
+
+            var ip = userInterface.GetIpAddress();
+
+            success = await client.ConnectAsync(ip, 25564);
+        }
+
+        return client;
+
+        // Try to connect using other players username as identification.
+        // If user already exists then add 1 to the end of username and try again. Repeat if needed with 2, 3, 4, etc.
+        // throw new NotImplementedException();
+    }
+
+    private async Task ListGroups()
+    {
+        
+    }
+
+
+
     public Task<TurnResult> PlayTurnAsync(IPlayer target, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
