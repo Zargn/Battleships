f69438562d120d1c7ef21ecd94075fda097d3259
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-25 11:37:57 +0100

    Added reference & create ILogger implementer

---
+++ BattleshipsFwServer/Logger.cs
@@ -0,0 +1,11 @@
+﻿using Unnamed_Networking_Plugin.Interfaces;
+
+namespace BattleshipsFwServer;
+
+public class Logger : ILogger
+{
+    public void Log(object sender, string message, LogType logType)
+    {
+        Console.WriteLine($"[{logType.ToString()}] {sender}: {message}");
+    }
+}
