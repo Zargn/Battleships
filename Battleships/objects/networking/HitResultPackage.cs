446e8751a112a6b569463fdf3100b69229c3499b
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-28 23:28:26 +0100

    Create HitResultPackage.cs

---
+++ Battleships/objects/networking/HitResultPackage.cs
@@ -0,0 +1,11 @@
+﻿namespace Battleships.objects.networking;
+
+public class HitResultPackage
+{
+    public HitResult HitResult { get; init; }
+
+    public HitResultPackage(HitResult hitResult)
+    {
+        HitResult = hitResult;
+    }
+}
