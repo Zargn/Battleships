507876652232eb2d2b108f0895ca25bf96885b27
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-28 23:28:28 +0100

    Create HitTilePackage.cs

---
+++ Battleships/objects/networking/HitTilePackage.cs
@@ -0,0 +1,11 @@
+﻿namespace Battleships.objects.networking;
+
+public class HitTilePackage
+{
+    public TargetCoordinates TargetCoordinates { get; init; }
+
+    public HitTilePackage(TargetCoordinates targetCoordinates)
+    {
+        TargetCoordinates = targetCoordinates;
+    }
+}
