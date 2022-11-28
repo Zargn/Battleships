cbb1681f0b7e8b8032969eb6d497a6fbf3504fcf
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-29 00:45:04 +0100

    Create ShipSunkPackage.cs

---
+++ Battleships/objects/networking/ShipSunkPackage.cs
@@ -0,0 +1,8 @@
+﻿using Unnamed_Networking_Plugin.Resources;
+
+namespace Battleships.objects.networking;
+
+public class ShipSunkPackage : Package
+{
+    // Doesn't really need to carry any information. Receiving the package is enough.
+}
