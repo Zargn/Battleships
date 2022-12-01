9c74b3817fb0e6127b6068ea59faf93533b3de96
Oskar Anderson <oskaranderson99@gmail.com>
2022-12-01 16:20:12 +0100

    Create RequestEndOfGameTilesPackage.cs

---
+++ Battleships/objects/networking/RequestEndOfGameTilesPackage.cs
@@ -0,0 +1,8 @@
+﻿using Unnamed_Networking_Plugin.Resources;
+
+namespace Battleships.objects.networking;
+
+public class RequestEndOfGameTilesPackage : Package
+{
+    // Doesn't really need to carry any information. Receiving the package is enough.
+}
