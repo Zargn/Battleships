6a88073160068e152d27e69e4aa5ca4ccbf6f712
Oskar Anderson <oskaranderson99@gmail.com>
2022-12-01 17:38:50 +0100

    Create ShipLocationsPackage.cs

---
+++ Battleships/objects/networking/ShipLocationsPackage.cs
@@ -0,0 +1,13 @@
+﻿using Unnamed_Networking_Plugin.Resources;
+
+namespace Battleships.objects.networking;
+
+public class ShipLocationsPackage : Package
+{
+    public TargetCoordinates[] Locations { get; init; }
+
+    public ShipLocationsPackage(TargetCoordinates[] locations)
+    {
+        this.Locations = locations;
+    }
+}
