36176eaa225fb071b7b42e88cebd5af6ebe5d6d7
Oskar Anderson <oskaranderson99@gmail.com>
2022-12-01 16:55:17 +0100

    Create BattleShipsWarningPackage.cs

---
+++ Battleships/objects/networking/BattleShipsWarningPackage.cs
@@ -0,0 +1,13 @@
+﻿using Unnamed_Networking_Plugin.Resources;
+
+namespace Battleships.objects.networking;
+
+public class BattleShipsWarningPackage : Package
+{
+    public string Message { get; }
+
+    public BattleShipsWarningPackage(string message)
+    {
+        Message = message;
+    }
+}
