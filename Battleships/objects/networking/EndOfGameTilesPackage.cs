aee332ed21e060b716c3bf0bd71a3a8f0e9985cb
Oskar Anderson <oskaranderson99@gmail.com>
2022-12-01 16:20:15 +0100

    Create EndOfGameTilesPackage.cs

---
+++ Battleships/objects/networking/EndOfGameTilesPackage.cs
@@ -0,0 +1,13 @@
+﻿using Unnamed_Networking_Plugin.Resources;
+
+namespace Battleships.objects.networking;
+
+public class EndOfGameTilesPackage : Package
+{
+    public Tile[,] Tiles { get; init; }
+
+    public EndOfGameTilesPackage(Tile[,] tiles)
+    {
+        Tiles = tiles;
+    }
+}
