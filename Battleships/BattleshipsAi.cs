76ed0d85074b648428219767e32b1c4285adccc2
Oskar Anderson <oskaranderson99@gmail.com>
2022-12-05 17:34:32 +0100

    BattleshipsAi.cs: Created event handler.

--- Battleships/BattleshipsAi.cs
+++ Battleships/BattleshipsAi.cs
@@ -4,8 +4,15 @@ namespace Battleships;
 
 public class BattleshipsAi
 {
+    private bool ShipTargeted { get; set; }
+    
     public TargetCoordinates CalculateNextShot()
     {
         throw new NotImplementedException();
+    }
+    
+    public void HandleShipSunkEvent(object? o, EventArgs e)
+    {
+        ShipTargeted = false;
     }
 }
