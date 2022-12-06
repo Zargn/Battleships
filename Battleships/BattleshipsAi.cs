6c2bbdbac222dca543a45e6e50ff17ab0529939e
Oskar Anderson <oskaranderson99@gmail.com>
2022-12-06 17:32:30 +0100

    BattleshipsAi.cs: Progress backup.

--- Battleships/BattleshipsAi.cs
+++ Battleships/BattleshipsAi.cs
@@ -1,18 +1,60 @@
-﻿using Battleships.objects;
+﻿using Battleships.Interfaces;
+using Battleships.objects;
 
 namespace Battleships;
 
 public class BattleshipsAi
 {
-    private bool ShipTargeted { get; set; }
+    private int arenaXSize;
+    private int arenaYSize;
     
-    public TargetCoordinates CalculateNextShot()
+    private bool ShipTargeted { get; set; }
+    private HashSet<int> shipHits = new HashSet<int>();
+
+    public BattleshipsAi(int arenaXSize, int arenaYSize)
     {
-        throw new NotImplementedException();
+        this.arenaXSize = arenaXSize;
+        this.arenaYSize = arenaYSize;
     }
     
+    // TODO: Probably not needed now that I already get the hitResult here.
     public void HandleShipSunkEvent(object? o, EventArgs e)
     {
         ShipTargeted = false;
     }
+    
+    
+    
+    public async Task<HitResult> PlayTurn(IPlayer targetPlayer, CancellationToken cancellationToken)
+    {
+        if (ShipTargeted)
+        {
+            
+        }
+        else
+        {
+            return FireAtRandomTile(targetPlayer, cancellationToken);
+        }
+    }
+
+    private HitResult FireAtRandomTile(IPlayer targetPlayer, CancellationToken cancellationToken)
+    {
+        while (true)
+        {
+            var targetLocation = GetRandomCoordinate();
+            var targetTile = targetPlayer.KnownArenaTiles[targetLocation.X, targetLocation.Y];
+            if (targetTile.Hit)
+                continue;
+            
+            if ()
+        }
+    }
+
+
+    private TargetCoordinates GetRandomCoordinate()
+    {
+        return new TargetCoordinates(Random.Shared.Next(arenaXSize), Random.Shared.Next(arenaYSize));
+    }
+    
+    private bool AdjacentToShip()
 }
