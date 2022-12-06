24807619032043faff6eb61e37d58853e6490f89
Oskar Anderson <oskaranderson99@gmail.com>
2022-12-06 22:31:56 +0100

    BattleshipsAi.cs: Further work on playturn

--- Battleships/BattleshipsAi.cs
+++ Battleships/BattleshipsAi.cs
@@ -7,14 +7,29 @@ public class BattleshipsAi
 {
     private int arenaXSize;
     private int arenaYSize;
+
+    // This is far from needed but I felt like experimenting to learn more about the subject.
+    private int xMult = 1;
+    private int yMult = 1;
     
     private bool ShipTargeted { get; set; }
-    private HashSet<int> shipHits = new HashSet<int>();
+    private HashSet<int> shipHits;
 
     public BattleshipsAi(int arenaXSize, int arenaYSize)
     {
         this.arenaXSize = arenaXSize;
         this.arenaYSize = arenaYSize;
+        
+        if (arenaXSize > arenaYSize)
+        {
+            xMult = arenaYSize;
+        }
+        else
+        {
+            yMult = arenaXSize;
+        }
+        
+        shipHits = new HashSet<int>(arenaXSize * arenaYSize);
     }
     
     // TODO: Probably not needed now that I already get the hitResult here.
@@ -45,8 +60,11 @@ public class BattleshipsAi
             var targetTile = targetPlayer.KnownArenaTiles[targetLocation.X, targetLocation.Y];
             if (targetTile.Hit)
                 continue;
+
+            if (AdjacentToShip(targetLocation, targetPlayer.KnownArenaTiles))
+                continue;
             
-            if ()
+            
         }
     }
 
@@ -55,6 +73,29 @@ public class BattleshipsAi
     {
         return new TargetCoordinates(Random.Shared.Next(arenaXSize), Random.Shared.Next(arenaYSize));
     }
-    
-    private bool AdjacentToShip()
+
+    private bool AdjacentToShip(TargetCoordinates location, Tile[,] knownTiles)
+    {
+        var searchCoordinate = location - new TargetCoordinates(1, 1);
+
+        for (var y = 0; y < 3; y++)
+        {
+            for (var x = 0; x < 3; x++)
+            {
+                searchCoordinate.X++;
+                if (!knownTiles[searchCoordinate.X, searchCoordinate.Y].OccupiedByShip) 
+                    continue;
+                
+                if (!shipHits.Contains(GetCoordinateHash(searchCoordinate)))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int GetCoordinateHash(TargetCoordinates value)
+    {
+        return value.X * xMult + value.Y * yMult;
+    }
 }
