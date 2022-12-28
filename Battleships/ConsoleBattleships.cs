f1475e8155ec192a7e7bf8afb88e0d8ddc1b2425
Oskar Anderson <oskaranderson99@gmail.com>
2022-12-28 17:26:27 +0100

    Create ConsoleBattleships.cs

---
+++ Battleships/ConsoleBattleships.cs
@@ -0,0 +1,20 @@
+﻿namespace Battleships;
+
+public class ConsoleBattleships
+{
+    public static async Task Main()
+    {
+        var userInterface = new ConsoleUserInterface();
+            
+        var game = new Game(userInterface);
+        while (true)
+        {
+            await game.Run();
+        
+            if (!userInterface.GetYesNoAnswer("Play again?", CancellationToken.None))
+            {
+                break;
+            }
+        }
+    }
+}
