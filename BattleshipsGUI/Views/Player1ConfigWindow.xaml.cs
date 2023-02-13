87bc980e621d87150cfafa62f0076665764dac38
Oskar Anderson <oskaranderson99@gmail.com>
2023-02-13 17:06:58 +0100

    Create Player1ConfigWindow

---
+++ BattleshipsGUI/Views/Player1ConfigWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace BattleshipsGUI.Views;
+
+public partial class Player1ConfigWindow : Window
+{
+    public Player1ConfigWindow()
+    {
+        InitializeComponent();
+    }
+}
