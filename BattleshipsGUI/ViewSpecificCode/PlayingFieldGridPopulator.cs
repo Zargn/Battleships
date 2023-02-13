3cdd46f7231395fbdd373f4585bcd9f1c03eaf7a
Oskar Anderson <oskaranderson99@gmail.com>
2023-02-13 17:05:40 +0100

    Create PlayingFieldGridPopulator.cs

---
+++ BattleshipsGUI/ViewSpecificCode/PlayingFieldGridPopulator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace BattleshipsGUI.ViewSpecificCode;
+
+public static class PlayingFieldGridCreator
+{
+    public static void PopulateGrid(Grid grid)
+    {
+        for (var x = 0; x < 10; x++)
+        {
+            for (var y = 0; y < 10; y++)
+            {
+                var button = new Button();
+                grid.Children.Add(button);
+                button.SetValue(Grid.RowProperty, y);
+                button.SetValue(Grid.ColumnProperty, x);
+                button.BorderBrush = new SolidColorBrush(Colors.DimGray);
+                button.Background = new SolidColorBrush();
+            }
+        }
+    }
+}
