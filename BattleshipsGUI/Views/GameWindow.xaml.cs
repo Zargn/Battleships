0fbb0407fb198989a38690c138c4b41ebf5354d0
Oskar Anderson <oskaranderson99@gmail.com>
2023-02-13 17:06:36 +0100

    GamwWindow.xaml.cs: Removed private PopulateGrid()

--- BattleshipsGUI/Views/GameWindow.xaml.cs
+++ BattleshipsGUI/Views/GameWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows.Controls;
 using System.Windows.Media;
 using BattleshipsGUI.ViewModels;
+using BattleshipsGUI.ViewSpecificCode;
 
 namespace BattleshipsGUI
 {
@@ -18,24 +19,8 @@ namespace BattleshipsGUI
 
         private void InitializeFields()
         {
-            PopulateGrid(Player1Field);
-            PopulateGrid(Player2Field);
-        }
-
-        private void PopulateGrid(Grid grid)
-        {
-            for (var x = 0; x < 10; x++)
-            {
-                for (var y = 1; y < 11; y++)
-                {
-                    var button = new Button();
-                    grid.Children.Add(button);
-                    button.SetValue(Grid.RowProperty, y);
-                    button.SetValue(Grid.ColumnProperty, x);
-                    button.BorderBrush = new SolidColorBrush(Colors.DimGray);
-                    button.Background = new SolidColorBrush();
-                }
-            }
+            PlayingFieldGridCreator.PopulateGrid(Player1Field);
+            PlayingFieldGridCreator.PopulateGrid(Player2Field);
         }
 
         private bool t;
