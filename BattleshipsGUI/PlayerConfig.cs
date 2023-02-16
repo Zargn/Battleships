60143d985f0a0eeae78deda90fb2b5a9092e9647
Oskar Anderson <oskaranderson99@gmail.com>
2023-02-16 19:13:41 +0100

    Create PlayerConfig.cs

---
+++ BattleshipsGUI/PlayerConfig.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BattleshipsGUI;
+
+public class PlayerConfig : INotifyPropertyChanged
+{
+    private bool player1UserControlled = true;
+    private bool player1CpuControlled;
+    
+    
+    public bool Player1UserControlled
+    {
+        get => player1UserControlled;
+        set
+        {
+            player1UserControlled = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    public bool Player1CpuControlled
+    {
+        get => player1CpuControlled;
+        set
+        {
+            player1CpuControlled = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
